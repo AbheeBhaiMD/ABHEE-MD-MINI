4b77e982cf8a1fa8c48e1dd9b78e0be55b1e67a3
ABHEE-MD <abdullahabeer387@gmail.com>
2026-07-17 21:55:38 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,12 @@
+module.exports = {
+    startimage: 'https://files.catbox.moe/y34d0t.jpg',
+    ownerNumber: process.env.OWNER_NUMBER || '923499850077',
+    botName: 'ABHEE MINI BOT',
+    ownerName: 'ABHEE',
+    whatsappChannel: 'https://whatsapp.com/channel/0029Vb7FZ4lBPzjb8g1Tux3',
+    tgOwnerId: process.env.OWNER_TELEGRAM_ID || '7520382907',
+    premiumUsers: [],
+    connectedBots: [],
+    version: '3.0.0',
+    prefix: '.'
+};
